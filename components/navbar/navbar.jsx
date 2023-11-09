4c870730f995cc24271c071d0e84fe69c8d3cc44
Manu <manurajbhar12@gmail.com>
2023-11-09 23:52:48 +0530

    Adding Pdf file

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
        initial={{opacity: 0 , x: 50}}
        animate={{opacity: 1, x: 0}}
        transition={{ duration: 1, ease: "easeOut", delay: 1.1 }}  className={styles.otherLinks}>
-                <Link className={styles.otherLink} href={"#"}>Resume</Link>
+                <Link className={styles.otherLink} target='_blank' href={"/files/Manu_Resume.pdf"}>Resume</Link>
                 </motion.div>
         </nav>
         <motion.div
